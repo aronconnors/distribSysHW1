656cce7c94c4d4b15431569ce9aa4896324c76d8
Aron Connors <aronconnors@Arons-MacBook-Air.local>
2024-10-08 23:28:39 -0400

    finished product

--- client.py
+++ client.py
@@ -1,5 +1,3 @@
-# more detailed code in the onenote
-
 import sys
 import socket
 import selectors
@@ -10,6 +8,7 @@ import threading
 sel = selectors.DefaultSelector()
 message = "start"
 
+# Establish connections, register the socket and its associated data
 def start_connections(host, port):
     server_addr = (host, port)
     print(f"Starting connection to {server_addr}")
@@ -23,32 +22,38 @@ def start_connections(host, port):
     )
     sel.register(sock, events, data=data)
 
+# Function constantly running to send and recieve data with the server
 def service_connection(key, mask):
      sock = key.fileobj
      data = key.data
+     # handle events where data is received from server
      if mask & selectors.EVENT_READ:
         recv_data = sock.recv(1024)
+        # specific case for closing connecions
         if recv_data.decode('utf-8') == 'Goodbye':
             print(recv_data.decode('utf-8'))
             print("\nClosing connection with server")
             sel.unregister(sock)
             sock.close()
         elif recv_data:
-            print(recv_data.decode('utf-8')+'\n')
-            #data.message = input("\nEnter a command: ")
+            print('\n'+recv_data.decode('utf-8')+'\n')
+    # handle events where data is sent to server
      if mask & selectors.EVENT_WRITE:
+        # if an outbound message is waiting, put it in outb
         if not data.outb and data.message:
             data.outb = data.message.encode('utf-8')
             data.message = None
+        # if there is data in outb, send as much of it as we can
         if data.outb:
             sent = sock.send(data.outb)
             data.outb = data.outb[sent:]
 
+# input threading
 def user_input(data):
     while True:
         data.message = input()
 
-
+# ensure the client is started with proper arguments
 if len(sys.argv) != 3:
     print(f"Usage: {sys.argv[0]} <host> <port>")
     sys.exit(1)
@@ -57,17 +62,18 @@ host, port = sys.argv[1:3]
 start_connections(host, int(port))
 
 try:
-    key = list(sel.get_map().values())[0]  # Get the key for the socket
+    # threading to avoid input locking command line
+    key = list(sel.get_map().values())[0]
     data = key.data
     input_thread = threading.Thread(target=user_input, args=(data,))
     input_thread.daemon = True
     input_thread.start()
+    # continuously service the connections
     while True:
         events = sel.select(timeout=1)
         if events:
             for key, mask in events:
                 service_connection(key, mask)
-        # Check for a socket being monitored to continue.
         if not sel.get_map():
             break
 except KeyboardInterrupt:
--- server.py
+++ server.py
@@ -1,6 +1,3 @@
-#for more detailed information about this code, go to the onenote file socket programming
-#not quite sure if all the code is in the right order as of right now
-
 import sys
 import socket
 import selectors
@@ -22,14 +19,17 @@ def get_history(id1,id2):
     history = chat_history.get(key,[])
     return "\n".join(history) if history else "No chat history between the two client ids."
 
+# register incoming connections from multiple clients
 def accept_wrapper(sock):
     conn, addr = sock.accept()
     print(f"Accepted connection from {addr}")
     conn.setblocking(False)
+    # iterate through 5 digit ID numbers
     newId = 10000
     while True:
         if newId in activeConnections:
             newId += 1
+        # register connection
         else:
             data = types.SimpleNamespace(addr=addr, id=newId, inb=b"", outb=b"")
             activeConnections[newId] = conn
@@ -37,45 +37,58 @@ def accept_wrapper(sock):
             sel.register(conn, events, data=data)
             break
 
+# continuously iterate 
 def service_connection(key, mask):
     sock = key.fileobj
     data = key.data
+    # when the event is reveiving data from a client
     if mask & selectors.EVENT_READ:
         recv_data = sock.recv(1024)
         if recv_data:
-            print(recv_data)
+            # decide what to do based on the command
+            # on start
             if recv_data.decode('utf-8') == 'start':
                 data.outb += 'Your unique ID: '.encode('utf-8') + str(key.data.id).encode('utf-8')
+            # list the active connections
             elif recv_data.decode('utf-8') == 'list':
                     users = ''
                     for key1, fileObj in sel.get_map().items():
                         if fileObj.data:
                             users = users + str(fileObj.data.id) + ', '
                     data.outb += 'Active user IDs: \n'.encode('utf-8') + users[:-2].encode('utf-8')
+            # foward a message to another client
             elif recv_data.decode('utf-8')[:7] == 'forward':
                 parts = recv_data.decode('utf-8').split()
-                print(parts)
                 destId = parts[1]
                 forwardMessage = " ".join(parts[2:])
                 if int(destId) in activeConnections.keys():
-                    sent = activeConnections[int(destId)].send(forwardMessage.encode('utf-8'))
+                    sent = activeConnections[int(destId)].send((f'FROM {data.id}: '+forwardMessage).encode('utf-8'))
                     data.outb += f'Message sent to {destId}'.encode('utf-8')
                     store_message(int(data.id),int(destId), forwardMessage)
                 else:
                     print(activeConnections.keys())
                     data.outb += f'{destId} is not an active user'.encode('utf-8')
+            # print a conversation history between two clients
             elif recv_data.decode('utf-8')[:7] == 'history':
                 parts = recv_data.decode('utf-8').split()
                 destId = parts[1]
                 data.outb += get_history(int(data.id),int(destId)).encode('utf-8')
-            if recv_data.decode('utf-8') == 'exit':
+            # exit the connection
+            elif recv_data.decode('utf-8') == 'help':
+                data.outb += 'COMMANDS:\n\nlist: list all active client IDs\nforward <<Client ID>> <<message>>: foward a message to another client\nhistory <<Client ID>>: print message history with another client\nexit: close the connection with server'.encode('utf-8')
+            elif recv_data.decode('utf-8') == 'exit':
                 data.outb += 'Goodbye'.encode('utf-8')
+            else:
+                data.outb += 'Invalid command. Use command "help" for a list of commands'.encode('utf-8')
+        # if there is no inbound data, close the connection
         else:
             print(f"Closing connection to {data.addr}")
             sel.unregister(sock)
             sock.close()
+    # on events where data is being sent to a client
     if mask & selectors.EVENT_WRITE:
         if data.outb:
+            # close connection
             if data.outb.decode('utf-8') == "Goodbye":
                 print(f"Saying goodbye to {data.addr}")
                 sent = sock.send(data.outb)
@@ -83,6 +96,7 @@ def service_connection(key, mask):
                 print(f"Closing connection to {data.addr}")
                 sel.unregister(sock)
                 sock.close()
+            # send the data to client
             else:
                 print(f"Echoing {data.outb.decode('utf-8')} to {data.addr}")
                 sent = sock.send(data.outb)
@@ -94,10 +108,12 @@ lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.listen()
 print(f"Listening on {(host, port)}")
+# nonblocking connections to allow for multiple client connecitons
 lsock.setblocking(False)
 sel.register(lsock, selectors.EVENT_READ, data=None)
 
 try:
+    # continuously parse through events and service the connections
     while True:
         events = sel.select(timeout=None)
         for key, mask in events:
